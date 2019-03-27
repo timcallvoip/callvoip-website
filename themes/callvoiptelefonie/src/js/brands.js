528b4ed8f604b3fa7f443791a2cfb844bc5945e4
Frank Spin <info@frankspin.nl>
2019-03-27 16:48:41 +0100

    Settings footer

--- themes/callvoiptelefonie/src/js/brands.js
+++ themes/callvoiptelefonie/src/js/brands.js
@@ -1,12 +1,9 @@
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
-import { faWhatsapp } from "@fortawesome/free-brands-svg-icons/faWhatsapp";
-import { faFacebook } from "@fortawesome/free-brands-svg-icons/faFacebook";
-import { faFacebookSquare } from "@fortawesome/free-brands-svg-icons/faFacebookSquare";
+import { faFacebookF } from "@fortawesome/free-brands-svg-icons/faFacebookF";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons/faTwitter";
-import { faYoutube } from "@fortawesome/free-brands-svg-icons/faYoutube";
-import { faLinkedin } from "@fortawesome/free-brands-svg-icons/faLinkedin";
+import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons/faLinkedinIn";
 
 
-library.add(faWhatsapp, faFacebook, faLinkedin,  faFacebookSquare, faTwitter, faYoutube);
+library.add(faFacebookF, faLinkedinIn, faTwitter);
 
 dom.watch();
