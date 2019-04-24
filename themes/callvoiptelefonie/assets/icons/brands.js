8fbbef0107723483e247bcb1cda66d9010f114be
Frank Spin <info@frankspin.nl>
2019-04-24 13:25:10 +0200

    youtube icon

--- themes/callvoiptelefonie/assets/icons/brands.js
+++ themes/callvoiptelefonie/assets/icons/brands.js
@@ -2,8 +2,8 @@ import { library, dom } from '@fortawesome/fontawesome-svg-core'
 import { faFacebookF } from "@fortawesome/free-brands-svg-icons/faFacebookF";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons/faTwitter";
 import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons/faLinkedinIn";
+import { faYoutube } from "@fortawesome/free-brands-svg-icons/faYoutube";
 
-
-library.add(faFacebookF, faLinkedinIn, faTwitter);
+library.add(faFacebookF, faLinkedinIn, faTwitter, faYoutube);
 
 dom.watch();
