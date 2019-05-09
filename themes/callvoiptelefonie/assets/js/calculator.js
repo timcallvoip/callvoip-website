e35cdfd063e4eb9842c5ce4361754289e4780365
Frank Spin <info@frankspin.nl>
2019-05-09 10:38:11 +0200

    x

--- themes/callvoiptelefonie/assets/js/calculator.js
+++ themes/callvoiptelefonie/assets/js/calculator.js
@@ -15,11 +15,58 @@ const state = {
   }
 }
 
-const basicFieldsRender = function (fields) {
+const getMonthlyTotal = function () {
+  let values = Object.values(state.monthly);
+  return values.reduce((partial_sum, a) => partial_sum + a,0);
+}
+
+const getOnetimeTotal = function () {
+  let values = Object.values(state.onetime);
+  return values.reduce((partial_sum, a) => partial_sum + a,0);
+}
+
+const renderMonthlyTotal = function () {
+  const totalFieldsMonthly = document.getElementById('total-fields-monthly');
+
+  const MonthlyTotal = `
+  <div class="flex mb-">
+  <div class="w-6">
+    &nbsp;
+  </div>
+  <div class="flex-1 px-2 text-right font-medium">Totaal:</div>
+  <div class="">${formatter.format(getMonthlyTotal())}</div>
+  </div>
+  `
+
+  totalFieldsMonthly.innerHTML = MonthlyTotal;
+}
+
+const renderOnetimeTotal = function () {
+  const totalFieldsOnetime = document.getElementById('total-fields-onetime');
+
+  const onetimeTotal = `
+  <div class="flex mb-">
+  <div class="w-6">
+    &nbsp;
+  </div>
+  <div class="flex-1 px-2 text-right font-medium">Totaal:</div>
+  <div class="">${formatter.format(getOnetimeTotal())}</div>
+  </div>
+  `
+
+  totalFieldsOnetime.innerHTML = onetimeTotal;
+}
+
+const renderBasicFields = function (fields) {
+
+  const basicFieldsMonthly = document.getElementById('basic-fields-monthly');
+  const basicFieldsOnetime = document.getElementById('basic-fields-onetime');
 
   let monthly = 0;
   let onetime = 0;
   state.basicFields = [];
+
+  /* loop over all basic fields */
 
   for (let item of fields) {
 
@@ -37,16 +84,14 @@ const basicFieldsRender = function (fields) {
 
   };
 
+  /* add totals to state */
+
   state.monthly.basicfields = monthly;
   state.onetime.basicFields = onetime;
 
-  console.log('state', state);
+  /* output Monthly templates rows */
 
-  const basicFieldsMonthly = document.getElementById('basic-fields-monthly');
-  const totalFieldsMonthly = document.getElementById('total-fields-monthly');
-
-
-  const MonthlyRows = `
+  const monthlyRows = `
   ${state.basicFields.map(item =>`
   ${(item.value > 0)  ? `
   <div class="flex mb-1">
@@ -57,21 +102,26 @@ const basicFieldsRender = function (fields) {
   ` : ''}`).join('')}
   `
 
-  const MonthlyTotal = `
-  ${(state.monthly.basicfields > 0)  ? `
-  <div class="flex mb-">
-  <div class="w-6">
-    &nbsp;
+  const onetimeRows = `
+  ${state.basicFields.map(item =>`
+  ${(item.value > 0)  ? `
+  <div class="flex mb-1">
+    <div class="w-6">${item.value}x</div>
+    <div class="flex-1 px-2">${item.name}</div>
+    <div class="">${formatter.format((item.value * item.price_onetime))}</div>
   </div>
-  <div class="flex-1 px-2 text-right font-medium">Totaal:</div>
-  <div class="">${formatter.format(state.monthly.basicfields)}</div>
-  </div>
-  ` : ''}`
+  ` : ''}`).join('')}
+  `
 
 
-  basicFieldsMonthly.innerHTML = MonthlyRows;
-  totalFieldsMonthly.innerHTML = MonthlyTotal;
+  basicFieldsMonthly.innerHTML = monthlyRows;
+  basicFieldsOnetime.innerHTML = onetimeRows;
 
+}
+
+const renderTotals = function() {
+  renderMonthlyTotal();
+  renderOnetimeTotal();
 }
 
 
@@ -80,17 +130,19 @@ document.addEventListener('DOMContentLoaded', function () {
   if (document.getElementById('basic-fields')) {
 
     const basicFields = document.getElementById('basic-fields').getElementsByTagName('input');
-    basicFieldsRender(basicFields);
+    renderBasicFields(basicFields);
 
     for (let basicField of basicFields) {
       basicField.addEventListener('change', function () {
-        basicFieldsRender(basicFields);
+        renderBasicFields(basicFields);
+        renderTotals();
       })
     }
 
   }
 
 
+  renderTotals();
 
 
 }, false);
