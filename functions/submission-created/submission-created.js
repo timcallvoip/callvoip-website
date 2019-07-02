cac06baf62e8e410c7a8bb3407702f31b9ddeace
Frank Spin <info@frankspin.nl>
2019-07-02 13:20:55 +0200

    x

--- functions/submission-created/submission-created.js
+++ functions/submission-created/submission-created.js
@@ -5,6 +5,29 @@ exports.handler = (event, context, callback) => {
     console.log(JSON.parse(event.body).payload.data);
     console.log('----------------Human Fields--------------')
     console.log(JSON.parse(event.body).payload.human_fields);
-    console.log('----------------Orderd Human Fields--------------')
-    console.log(JSON.parse(event.body).payload.orderd_human_fields);
+    console.log('----------------Ordered Human Fields--------------')
+    console.log(JSON.parse(event.body).payload.ordered_human_fields);
+    console.log('----------------Form Name--------------')
+    console.log(JSON.parse(event.body).payload.form_name);
+
+    const sgMail = require('@sendgrid/mail');
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+
+    const msg = {
+      to: "info@frankspin.nl",
+      from: 'info@callvoiptelefonie.nl',
+      subject: 'Inzending contactformulier ontvangen',
+
+      // template id from sendgrid
+      templateId: 'd-5f1602c68c8a42919ddf340e285386e3',
+      substitutionWrappers: ['{{', '}}'],
+      substitutions: {
+        first_name: "test voornaam"
+      }
+    };
+
+    return sgMail.send(msg);
+
+
 };
