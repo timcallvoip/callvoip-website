3ef9a57066906191cf2b26054d7b2603a736bfd7
Frank Spin <info@frankspin.nl>
2019-07-02 10:29:41 +0200

    x

--- functions/submission-created/submission-created.js
+++ functions/submission-created/submission-created.js
@@ -1,5 +1,10 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = (event, context, callback) => {
     console.log('called', event)
-    console.log(JSON.parse(event.body).payload);
+    console.log('----------------DATA--------------')
+    console.log(JSON.parse(event.body).payload.data);
+    console.log('----------------Human Fields--------------')
+    console.log(JSON.parse(event.body).payload.human_fields);
+    console.log('----------------Orderd Human Fields--------------')
+    console.log(JSON.parse(event.body).payload.orderd_human_fields);
 };
