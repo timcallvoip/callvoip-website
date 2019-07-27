4ed97896c513f115611adda9b90098bfd10e87ff
Frank Spin <info@frankspin.nl>
2019-07-27 17:26:52 +0200

    x

--- functions/submission-created/submission-created.js
+++ functions/submission-created/submission-created.js
@@ -1,20 +1,20 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = (event, context, callback) => {
-    console.log('called', event)
-    console.log('----------------DATA--------------')
-    console.log(JSON.parse(event.body).payload.data);
-    console.log('----------------Human Fields--------------')
-    console.log(JSON.parse(event.body).payload.human_fields);
-    console.log('----------------Ordered Human Fields--------------')
-    console.log(JSON.parse(event.body).payload.ordered_human_fields);
-    console.log('----------------Form Name--------------')
-    console.log(JSON.parse(event.body).payload.form_name);
+    // console.log('called', event)
+    // console.log('----------------DATA--------------')
+    // console.log(JSON.parse(event.body).payload.data);
+    // console.log('----------------Human Fields--------------')
+    // console.log(JSON.parse(event.body).payload.human_fields);
+    // console.log('----------------Ordered Human Fields--------------')
+    // console.log(JSON.parse(event.body).payload.ordered_human_fields);
+    // console.log('----------------Form Name--------------')
+    // console.log(JSON.parse(event.body).payload.form_name);
 
     const data = JSON.parse(event.body).payload.data;
     const form_name = JSON.parse(event.body).payload.form_name;
     const fields = JSON.parse(event.body).payload.ordered_human_fields;
 
-    console.log('THIS IS THE DATA', data)
+    // console.log('THIS IS THE DATA', data)
 
     const sgMail = require('@sendgrid/mail');
     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
@@ -22,16 +22,31 @@ exports.handler = (event, context, callback) => {
     const defaultTemplate = 'd-5f1602c68c8a42919ddf340e285386e3';
     const internalTemplate = 'd-b8915fd3b5f149ccbbcb6b469aecc71d';
 
-    console.log('-----------------------------------')
-    console.log('emailadres is', data.email)
-    console.log('formlayout is', data.formlayout)
-    console.log('form_name is', form_name)
-    console.log('-----------------------------------')
+
+    const toEmail = '';
+
+    if (data.fromto == 'dev') {
+      toEmail = 'info@frankspin.nl'
+    }
+
+    if (data.fromto == 'info') {
+      toEmail = 'info@callvoip.nl'
+    }
+
+    if (data.fromto == 'offerte') {
+      toEmail = 'offerte@callvoip.nl'
+    }
+
+    if (data.fromto == 'aanvragen') {
+      toEmail = 'aanvragen@callvoip.nl'
+    }
+
+
 
 
     const msgClient = {
       to: data.email,
-      from: data.formto || 'info@callvoiptelefonie.nl',
+      from: toEmail || 'aanvragen@callvoip.nl',
       subject: 'Inzending formulier Callvoip',
 
       // template id from sendgrid
@@ -43,8 +58,8 @@ exports.handler = (event, context, callback) => {
     };
 
     const msgInternal = {
-      to: data.formto,
-      from: data.email || 'info@callvoiptelefonie.nl',
+      to: toEmail,
+      from: data.email || 'aanvragen@callvoip.nl',
       subject: 'Inzending formulier callvoip.nl',
 
       // template id from sendgrid
