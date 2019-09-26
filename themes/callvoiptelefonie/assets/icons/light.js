5f184e6e4d6030ebc41e5a4e8372f3dcc7459c30
Frank Spin <info@frankspin.nl>
2019-09-26 14:43:33 +0200

    x

--- themes/callvoiptelefonie/assets/icons/light.js
+++ themes/callvoiptelefonie/assets/icons/light.js
@@ -68,8 +68,9 @@ import { faShoppingCart } from "@fortawesome/pro-light-svg-icons/faShoppingCart"
 import { faNetworkWired } from "@fortawesome/pro-light-svg-icons/faNetworkWired";
 import { faCommentAltDots } from "@fortawesome/pro-light-svg-icons/faCommentAltDots";
 import { faDesktopAlt } from "@fortawesome/pro-light-svg-icons/faDesktopAlt";
+import { faDotCircle } from "@fortawesome/pro-light-svg-icons/faDotCircle";
 
-library.add(faCloud, faClock, faHeartbeat, faLaptop, faTty, faIdCard, faCheckCircle, faWindowRestore, faNetworkWired, faCommentAltDots, faDesktopAlt, faCut, faAd, faTooth, faAbacus, faShoppingCart, faTimesCircle, faServer, faMobileAndroid,faMobileAndroidAlt, faAngleDown, faWindowRestore, faMousePointer, faHandPointer,  faRandom, faDeaf, faSortNumericDown, faArrowsH, faUsers, faFilePdf, faBell, faUnlock, faFileAudio, faCog, faSpinner, faMoneyBill, faExclamationCircle, faFax, faCalculator, faTrophy, faRocket, faSearch, faEnvelope, faFile, faMobile, faGlobe, faWifi, faBars, faUser, faMegaphone, faBoxOpen, faCommentAlt, faUserLock, faMobileAlt, faPhoneVolume, faCodeBranch, faCommentAltSmile, faFileSignature, faPiggyBank, faFileContract, faBoxesAlt, faAngleRight, faHome, faBuilding, faComments, faPhone, faDesktop, faTabletAlt,faHeadset);
+library.add(faCloud, faClock, faHeartbeat, faDotCircle, faLaptop, faTty, faIdCard, faCheckCircle, faWindowRestore, faNetworkWired, faCommentAltDots, faDesktopAlt, faCut, faAd, faTooth, faAbacus, faShoppingCart, faTimesCircle, faServer, faMobileAndroid,faMobileAndroidAlt, faAngleDown, faWindowRestore, faMousePointer, faHandPointer,  faRandom, faDeaf, faSortNumericDown, faArrowsH, faUsers, faFilePdf, faBell, faUnlock, faFileAudio, faCog, faSpinner, faMoneyBill, faExclamationCircle, faFax, faCalculator, faTrophy, faRocket, faSearch, faEnvelope, faFile, faMobile, faGlobe, faWifi, faBars, faUser, faMegaphone, faBoxOpen, faCommentAlt, faUserLock, faMobileAlt, faPhoneVolume, faCodeBranch, faCommentAltSmile, faFileSignature, faPiggyBank, faFileContract, faBoxesAlt, faAngleRight, faHome, faBuilding, faComments, faPhone, faDesktop, faTabletAlt,faHeadset);
 
 
 dom.watch();
